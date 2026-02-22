bae257912618c933cd20079b27c1a6f28792c6fc
Nikhilesh K S <nickstack09@gmail.com>
2026-02-22 21:32:57 +0530

    modified the code

--- day_cookie.pl
+++ day_cookie.pl
@@ -1,29 +1,26 @@
-##!C:\xampp\perl\bin\perl.exe
-use strict;
-use warnings;
+#!C:\xampp\perl\bin\perl.exe
 use CGI qw(:standard);
 
-my @last_day = split(' ', cookie('last_day') || '');
-my @weekdays = qw(Sunday Monday Tuesday Wednesday Thursday Friday Saturday);
-my @months = qw(January February March April May June July August September October November December);
-
-my $day_of_week = $weekdays[(localtime)[6]];
-my $day_of_month = (localtime)[3];
-my $month = $months[(localtime)[4]];
-
-my @day_stuff = ($day_of_week, $day_of_month, $month);
-my $day_cookie = cookie(-name => 'last_day', 
+@last_day = cookie('last_day');
+@weekdays = qw(Sun Mon Tue Wed Thu Fri Sat);
+@months = qw(Jan Feb Mar Apr May Jun Jul Aug Sep Oct Nov Dec);
+@time = localtime();
+$day_of_week = $weekdays[$time[6]];
+$month = $months[$time[4]];
+$day_of_month = $time[3];
+@day_stuff = ($day_of_week, $month, $day_of_month);
+$day_cookie = cookie(-name => 'last_day', 
                         -value => join(' ', @day_stuff), 
                         -expires => '+5d');
 
 print header(-cookie => $day_cookie);
-print start_html('This is day_cookie.pl');
+print start_html('This is day_cookie2.pl');
 
-if (scalar(@last_day) == 0) {
-    print "Welcome to our website for the first time!<br>";
+if (scalar @last_day == 0) {
+    print "Welcome to you on your first visit to our site<br>";
 } else {
-    ($day_of_week, $day_of_month, $month) = @last_day;
-    print "Welcome back! Your last visit was on $day_of_week $day_of_month $month.<br>";
+    ($day_of_week, $month, $day_of_month) = @last_day;
+    print "Welcome back!<br/> Your last visit on ", $day_of_week, " ", $month, " ", $day_of_month, "<br>";
 }
 
 print end_html();
